3ee658878ec97eb3a3afe0b713619761e0fdccac
Furkan Bulut <furkanbtng@gmail.com>
2026-01-13 00:15:43 +0300

    API Key

--- YurttaYe.Services/obj/Debug/net8.0/YurttaYe.Services.AssemblyInfo.cs
+++ YurttaYe.Services/obj/Debug/net8.0/YurttaYe.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YurttaYe.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9426889e3a1dec35d710b936a8359da2933a546b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84619692b3f9bc3b4050719ca33874c24a135fc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("YurttaYe.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YurttaYe.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- YurttaYe.Tests/obj/Debug/net8.0/YurttaYe.Tests.AssemblyInfo.cs
+++ YurttaYe.Tests/obj/Debug/net8.0/YurttaYe.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YurttaYe.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da18fe235e2e715847924015b716208edb096e00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84619692b3f9bc3b4050719ca33874c24a135fc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("YurttaYe.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YurttaYe.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
